89bce30cd1678295bcf58fbcaa5b56eee1d0247f
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2022-04-06 23:46:29 +0200

    rename function in quick-notes-completion script

--- quick-notes-completion.bash
+++ quick-notes-completion.bash
@@ -4,7 +4,7 @@
 # Bash auto-completion for quick-notes
 #
 
-_ggscripts_quicknotes_complete()
+_quicknotes_complete()
 {
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
@@ -20,7 +20,7 @@ _ggscripts_quicknotes_complete()
   fi
 }
 
-complete -F _ggscripts_quicknotes_complete learned
-complete -F _ggscripts_quicknotes_complete questions
-complete -F _ggscripts_quicknotes_complete installed
-complete -F _ggscripts_quicknotes_complete bookmark
+complete -F _quicknotes_complete learned
+complete -F _quicknotes_complete questions
+complete -F _quicknotes_complete installed
+complete -F _quicknotes_complete bookmark
